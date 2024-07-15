2c3c8739cb858894cff3c71ec4536aa084bb90ba
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-07-15 10:14:06 +0530

    tesinting with redux

--- src/utils/admin/PrivateRoute.tsx
+++ src/utils/admin/PrivateRoute.tsx
@@ -1,9 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import Cookies from 'js-cookie';
+import { RootState } from "../../Redux/store/store";
+import { useSelector } from "react-redux";
 
 const PrivateRoute = () => {
+    const isAuthenticated = useSelector((store: RootState) => store.UserData.isAuthenticated);
     const token = Cookies.get('token');
-    const isAuthenticated = token && token.trim() !== '';
+    console.log(token, isAuthenticated, 'admin');
+    // const isAuthenticated = token && token.trim() !== '';
     return isAuthenticated ? <Outlet /> : <Navigate to="/" />
 }
 export default PrivateRoute
