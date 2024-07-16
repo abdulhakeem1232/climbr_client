ef14372b2be1eb17b3874befc2e6d007aac0953c
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-07-16 12:02:47 +0530

    webrtc

--- src/utils/socket/Socket.ts
+++ src/utils/socket/Socket.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 // const socket = io('http://localhost:8080');
 // const socket = io('http://localhost:30080');
-const socket = io('https://climbrserver.online');
+const socket = io('https://climbrserver.online', { secure: true });
 
 
 
