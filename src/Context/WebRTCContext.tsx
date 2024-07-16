8a04cbed3f226ad7103296318a58c968903a0296
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-07-16 13:02:57 +0530

    testing

--- src/Context/WebRTCContext.tsx
+++ src/Context/WebRTCContext.tsx
@@ -36,13 +36,21 @@ export const WebRTCProvider: React.FC<{ children: React.ReactNode }> = ({ childr
         if (!peerConnection.current) {
             peerConnection.current = new RTCPeerConnection({
                 iceServers: [
-                    { urls: 'stun:stun.l.google.com:19302' },
                     {
-                        urls: 'turn:global.turn.twilio.com:3478?transport=udp',
-                        username: process.env.REACT_APP_TWILIO_ACCOUNT_SID || '',
-                        credential: process.env.REACT_APP_TWILIO_AUTH_TOKEN || ''
-                    }
-                ]
+                        urls: [
+                            "stun:stun.l.google.com:19302",
+                            "stun:global.stun.twilio.com:3478",
+                        ],
+                    },
+                ],
+                // iceServers: [
+                //     { urls: 'stun:stun.l.google.com:19302' },
+                //     {
+                //         urls: 'turn:global.turn.twilio.com:3478?transport=udp',
+                //         username: process.env.REACT_APP_TWILIO_ACCOUNT_SID || '',
+                //         credential: process.env.REACT_APP_TWILIO_AUTH_TOKEN || ''
+                //     }
+                // ]
             });
 
             peerConnection.current.ontrack = (event) => {
@@ -76,22 +84,22 @@ export const WebRTCProvider: React.FC<{ children: React.ReactNode }> = ({ childr
 
         if (!peerConnection.current) {
             peerConnection.current = new RTCPeerConnection({
-                // iceServers: [
-                //     {
-                //         urls: [
-                //             "stun:stun.l.google.com:19302",
-                //             "stun:global.stun.twilio.com:3478",
-                //         ],
-                //     },
-                // ],
                 iceServers: [
-                    { urls: 'stun:stun.l.google.com:19302' },
                     {
-                        urls: 'turn:global.turn.twilio.com:3478?transport=udp',
-                        username: process.env.REACT_APP_TWILIO_ACCOUNT_SID || '',
-                        credential: process.env.REACT_APP_TWILIO_AUTH_TOKEN || ''
-                    }
-                ]
+                        urls: [
+                            "stun:stun.l.google.com:19302",
+                            "stun:global.stun.twilio.com:3478",
+                        ],
+                    },
+                ],
+                // iceServers: [
+                //     { urls: 'stun:stun.l.google.com:19302' },
+                //     {
+                //         urls: 'turn:global.turn.twilio.com:3478?transport=udp',
+                //         username: process.env.REACT_APP_TWILIO_ACCOUNT_SID || '',
+                //         credential: process.env.REACT_APP_TWILIO_AUTH_TOKEN || ''
+                //     }
+                // ]
             });
 
             peerConnection.current.ontrack = (event) => {
