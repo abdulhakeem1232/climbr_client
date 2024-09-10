da231c964aa792b23cff47ca4c26bb9572b1e71c
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-09-11 00:37:26 +0530

    testing

--- src/utils/socket/Socket.ts
+++ src/utils/socket/Socket.ts
@@ -3,7 +3,6 @@ import io from 'socket.io-client';
 // const socket = io('http://localhost:8080');
 // const socket = io('http://localhost:30080');
 const socket = io('https://climbrserver.site', {
-    transports: ['websocket'],
     withCredentials: true
 });
 
